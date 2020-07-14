e3913beda8c22624fbe91f050eebd349f8f44470
tomwilde <tom@furblegurks.com>
2020-07-14 17:11:13 +0100

    allow the option to not use cuda by-default  (#5)
    * Added a CPU only option.
    
    * The names have been chnaged to protect the innocent
    
    Co-authored-by: Thomas Wilde <t.e.wilde@gmail.com>

--- pipelines.py
+++ pipelines.py
@@ -23,6 +23,7 @@ class QGPipeline:
         ans_model: PreTrainedModel,
         ans_tokenizer: PreTrainedTokenizer,
         qg_format: str,
+        use_cuda: bool
     ):
         self.model = model
         self.tokenizer = tokenizer
@@ -32,7 +33,8 @@ class QGPipeline:
 
         self.qg_format = qg_format
 
-        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.device = "cuda" if torch.cuda.is_available() and use_cuda else "cpu"
+        print(self.device)
 
         self.model.to(self.device)
 
@@ -202,12 +204,13 @@ class E2EQGPipeline:
         self,
         model: PreTrainedModel,
         tokenizer: PreTrainedTokenizer,
+        use_cuda: bool
     ) :
 
         self.model = model
         self.tokenizer = tokenizer
 
-        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.device = "cuda" if torch.cuda.is_available() and use_cuda else "cpu"
         self.model.to(self.device)
 
         assert self.model.__class__.__name__ in ["T5ForConditionalGeneration", "BartForConditionalGeneration"]
@@ -311,6 +314,7 @@ def pipeline(
     qg_format: Optional[str] = "highlight",
     ans_model: Optional = None,
     ans_tokenizer: Optional[Union[str, PreTrainedTokenizer]] = None,
+    use_cuda: Optional = True,
     **kwargs,
 ):
     # Retrieve the task
@@ -377,8 +381,8 @@ def pipeline(
                 ans_model = AutoModelForSeq2SeqLM.from_pretrained(ans_model)
     
     if task == "e2e-qg":
-        return task_class(model=model, tokenizer=tokenizer)
+        return task_class(model=model, tokenizer=tokenizer, use_cuda=use_cuda)
     elif task == "question-generation":
-        return task_class(model=model, tokenizer=tokenizer, ans_model=ans_model, ans_tokenizer=ans_tokenizer, qg_format=qg_format)
+        return task_class(model=model, tokenizer=tokenizer, ans_model=ans_model, ans_tokenizer=ans_tokenizer, qg_format=qg_format, use_cuda=use_cuda)
     else:
-        return task_class(model=model, tokenizer=tokenizer, ans_model=model, ans_tokenizer=tokenizer, qg_format=qg_format)
+        return task_class(model=model, tokenizer=tokenizer, ans_model=model, ans_tokenizer=tokenizer, qg_format=qg_format, use_cuda=use_cuda)
