191bf4ffdd3806ef0f85ad106a6d5925e58d8362
Erdi Tac <erditac@gmail.com>
2021-09-29 08:38:56 +0800

    trainer.is_world_master is depreciated
    Replace trainer.is_world_master() with trainer.is_world_process_zero()

--- run_qg.py
+++ run_qg.py
@@ -202,7 +202,7 @@ def main(args_file=None):
         trainer.save_model()
         # For convenience, we also re-save the tokenizer to the same directory,
         # so that you can share your model easily on huggingface.co/models =)
-        if trainer.is_world_master():
+        if trainer.is_world_process_zero():
             tokenizer.save_pretrained(training_args.output_dir)
 
     # Evaluation
